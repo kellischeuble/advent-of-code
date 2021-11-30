b11b83a6357b4f72e93951f7d50359a032b831a4
Kelli Scheuble <kelli@abett.com>
2021-11-30 13:02:08 -0800

    Update day 10

--- 2020/days 10-19/day 10/solution.py
+++ 2020/days 10-19/day 10/solution.py
@@ -1,19 +1,19 @@
-differences = {}
+def part_one():
+    jolts = [28,33,18,42,31,14,46,20,48,47,24,23,49,45,19,38,39,11,1,32,25,35,8,17,7,9,4,2,34,10,3]
+    jolts = sorted(jolts)
+    jolts.append(jolts[-1] + 3)
+    differences = dict()
 
-with open("jolts.txt", "r") as jolts:
-    jolts = sorted([int(jolt.rpartition("\n")[0]) for jolt in jolts])
     prev = 0
 
-    # add built-in joltage adapter
-    jolts.append(jolts[-1] + 3)
-
     for jolt in jolts:
-        difference = abs(prev - jolt)
-        if not difference in differences:
+        difference = jolt - prev
+        if difference > 3:
+            print("GREATER THAN 3")
+        if difference not in differences:
             differences[difference] = 0
         differences[difference] += 1
         prev = jolt
 
-    print("ANSWER TO PART ONE:", differences[1] * differences[3])
-
+    print("PART ONE ANSWER", differences[3] * differences[1])
 
